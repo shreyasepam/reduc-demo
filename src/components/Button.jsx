50c222c3b2bbe5918a04c66c0c09faff0b59e4d0
shreyasepam <shreyas_bm@epam.com>
2024-10-19 21:44:05 +0530

    style to string

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+const default_className = "focus:shadow-outline transform font-semibold transition duration-150 ease-in-out focus:outline-none active:scale-95"
+
 export default function Button({ children, onClick, className }) {
   return (
     <button
-      className={` ${className} focus:shadow-outline transform font-semibold transition duration-150 ease-in-out focus:outline-none active:scale-95 `}
+      className={`${default_className} ${className} `}
       onClick={onClick}
     >
       {children}
